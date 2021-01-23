5bf5d7c2339134d8524470f521bcdc60786e60dc
connect.anirudh@gmail.com <connect.anirudh@gmail.com>
2021-01-24 01:22:23 +0530

    rename basepath as location

--- src/creator/project.py
+++ src/creator/project.py
@@ -6,9 +6,9 @@ import click
 from creator.managers import Manager as _Manager
 
 
-def create_project_path(name, basepath=None):
-    if basepath is not None:
-        return os.path.join(basepath, name)
+def create_project_path(name, location=None):
+    if location is not None:
+        return os.path.join(location, name)
     else:
         return os.path.join(name)
 
@@ -29,13 +29,13 @@ def project():
 
 
 @project.command()
-@click.argument("name")
-@click.option("--source")
-@click.option("--tests")
-@click.option("--resources")
-@click.option("--basepath")
-def create(name, source=None, tests=None, resources=None, basepath=None):
-    project_path = create_project_path(name, basepath)
+@click.argument("name", "Name of the project.")
+@click.option("--source", "-s", help="Name for the source directory.")
+@click.option("--tests", "-t", help="Name for the testcase directory.")
+@click.option("--resources", "-r", help="Name for the resources directory.")
+@click.option("--location", "-l", help="Parent directory location of the project.")
+def create(name, source=None, tests=None, resources=None, location=None):
+    project_path = create_project_path(name, location)
     paths = create_path_list(project_path, [source, tests, resources])
     managerClass = _Manager(name, project_path, paths)
     managerClass.init_project()
@@ -43,10 +43,10 @@ def create(name, source=None, tests=None, resources=None, basepath=None):
 
 
 @project.command()
-@click.argument("name")
-@click.option("--basepath")
-def destroy(name, basepath=None):
-    project_path = create_project_path(name, basepath)
+@click.argument("name", "Name of the project.")
+@click.option("--location", "-l", help="Parent directory location of the project.")
+def destroy(name, location=None):
+    project_path = create_project_path(name, location)
     managerClass = _Manager(name, project_path)
     managerClass.destroy_project()
 
